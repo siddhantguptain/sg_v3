e015d02c11f3386bd914f9e0a8b03171733c950c
Siddhant <me@siddhantgupta.in>
2024-01-22 04:30:36 +0530

    Linked the HOME Page and the Panels in FLex 8 & 0.5 withthe main APP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+import HOME from './components/Home/Home.js';
+import PANEL_LEFT from './components/SidePANELS/Panel_LEFT/Panel_LEFT.js';
+import PANEL_RIGHT from './components/SidePANELS/Panel_RIGHT/Panel_RIGHT.js';
+
 function App() {
   return (
-    <div >
-       Let Start 22 Jan 2024
+    <div className="AppContainer">
+        <div className="LEFTContainer">
+            <PANEL_LEFT />
+        </div>
+        <div className="MAINContainer">
+            <HOME />
+        </div>
+        <div className="RIGHTContainer">
+            <PANEL_RIGHT/>
+        </div>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from "./HOME.module.css";
+import style from "./Home.module.css";
 const HOME  = () =>{
     return(
         <div className={style.container}>
