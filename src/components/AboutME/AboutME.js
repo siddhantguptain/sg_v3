d793bb9ca42ecd4b5b7e50dcccf4c11f6d111b52
Siddhant <me@siddhantgupta.in>
2024-01-22 05:20:27 +0530

    Updated the About Page

--- src/components/AboutME/AboutME.js
+++ src/components/AboutME/AboutME.js
@@ -3,7 +3,12 @@ import style from "./AboutME.module.css";
 const ABOUT_ME  = () =>{
     return(
         <div className={style.container}>
-            ABOUT_ME
+            <div className={style.heading}>
+                About Me
+            </div>
+            <div className={style.paragraph}>
+            A highly innovative individual with a keen interest in developing and deploying new projects on World Wide Web Ability to integrate market needs into technical solutions. Looking to begin a career as an entry-level software developer with a reputable company that is focused on innovation.
+            </div>
         </div>
     );
 }
