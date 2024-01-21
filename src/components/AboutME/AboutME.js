64c722d3e484ad012fa0735e30c81a6d507b56c7
Siddhant <me@siddhantgupta.in>
2024-01-22 05:02:31 +0530

    Added & Link the ABOUT_ME Setup

---
+++ src/components/AboutME/AboutME.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from "./AboutME.module.css";
+const ABOUT_ME  = () =>{
+    return(
+        <div className={style.container}>
+            ABOUT_ME
+        </div>
+    );
+}
+export default ABOUT_ME;
