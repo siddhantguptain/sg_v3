cf223ee6759e618e18b9a0ac870717786105ccb6
Siddhant <me@siddhantgupta.in>
2024-01-22 05:02:57 +0530

    Temp UPdated the Panels

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
-import HOME from './components/Home/Home.js';
 import PANEL_LEFT from './components/SidePANELS/Panel_LEFT/Panel_LEFT.js';
 import PANEL_RIGHT from './components/SidePANELS/Panel_RIGHT/Panel_RIGHT.js';
+
+import HOME from './components/Home/Home.js';
+import ABOUT_ME from './components/AboutME/AboutME.js';
+
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
         </div>
         <div className="MAINContainer">
             <HOME />
+            <ABOUT_ME />
         </div>
         <div className="RIGHTContainer">
             <PANEL_RIGHT/>
