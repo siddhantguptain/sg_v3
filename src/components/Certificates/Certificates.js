0480ee4ab765d6a1e60a1b72bc59114667efdaef
Siddhant <me@siddhantgupta.in>
2024-01-24 02:24:16 +0530

    Checked the Img with Variable

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import style from "./Certificates.module.css";
+
+import MERN from "../../Assets/MERN.png";
+import REACTJS from "../../Assets/REACTJS.png";
+import JS from "../../Assets/JS.png";
+import WEBDEV from "../../Assets/WEBDEV.png";
+
+const CertificatesDATA =[
+    {
+        src:MERN,
+        title:"The MERN FullStack Guide",
+    },
+    {
+        src:REACTJS,
+        title:"React - The Complete Guide",
+    },
+    {
+        src:JS,
+        title:"JavaScript - Zero to Expert!",
+    },
+    {
+        src:WEBDEV,
+        title:"Web Development BootCamp",
+    },
+] ;
+
 const CERTIFICATES  = () =>{
     return(
         <div className={style.container}>
             <div className={style.heading}>
                 Certificates
             </div>
-            <div className={style.displayContainer}></div>
+            <div className={style.displayContainer}>
+                    <img src={JS} />
+            </div>
         </div>
     );
 }
