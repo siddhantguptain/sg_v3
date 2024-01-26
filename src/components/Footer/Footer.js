b372ef0bf85dccf7dc513e95a9f6a677b7b2506f
Siddhant <me@siddhantgupta.in>
2024-01-27 02:40:14 +0530

    Upgraded the Footer Styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,22 @@ import style from "./Footer.module.css";
 const FOOTER  = () =>{
     return(
         <div className={style.container}>
-                Footer
+            <h3 className={style.Head}>Siddhant Gupta</h3>
+            <h4 className={style.mail}>me@siddhantgupta.in</h4>
+            <div className={style.navItems}>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Certification</li>
+                    <li>Project</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className={style.CC}> © 2024 Designed and Build by SiddhantGupta</div>
+
+
+            
+
         </div>
     );
 }
